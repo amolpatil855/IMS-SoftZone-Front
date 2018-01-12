c21345316eb67d2b2ec44281ebc5a3891535b1dd
amolpatil855 <amol.patil@eternussolutions.com>
2018-01-12 21:22:29 +0530

    Changs added for publish

--- src/app/theme/pages/default/features/users/users-list/users-list.component.ts
+++ src/app/theme/pages/default/features/users/users-list/users-list.component.ts
@@ -59,6 +59,169 @@ export class UsersListComponent implements OnInit {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
   }
+  moreNextPages() {
+    if (this.boundryEnd + 1 <= this.pages) {
+      this.boundryStart = this.boundryEnd + 1;
+      this.currentPageNumber = this.boundryStart;
+      if (this.boundryEnd + this.boundry >= this.pages) {
+        this.boundryEnd = this.pages;
+      } else {
+        this.boundryEnd = this.boundryEnd + this.boundry;
+      }
+      //this.getQueryDataCount();
+    }
+    //this.generateCount();
 
-  
+
+  }
+
+  morePreviousPages() {
+    if (this.boundryStart - this.boundry > 0) {
+      this.boundryStart = this.boundryStart - this.boundry;
+      this.boundryEnd = this.boundryStart + this.boundry - 1;
+      this.currentPageNumber = this.boundryEnd;
+      //this.getQueryDataCount();
+    }
+  }
+
+  pageSizeChanged(size) {
+    this.perPage = size;
+    this.currentPos = 0;
+    this.currentPageNumber = 1;
+    //this.getQueryDataCount();
+  }
+
+  visitFirstPage() {
+    if (this.boundryStart > this.boundry) {
+      this.currentPos = 0;
+      this.currentPageNumber = 1;
+      this.boundryStart = 1;
+      this.boundryEnd = this.boundry;
+      //this.generateCount();
+      this.setDisplayPageNumberRange();
+      //this.getAllRoles();
+    }
+  }
+
+  visitLastPage() {
+    for (var index = 0; this.currentPos + this.perPage < this.total; index++) {
+      this.currentPos += this.perPage;
+      this.currentPageNumber++;
+    }
+    this.boundryStart = 1;
+    this.boundryEnd = this.boundry;
+    for (var index = 0; this.boundryEnd + 1 <= this.pages; index++) {
+      this.boundryStart = this.boundryEnd + 1;
+
+      if (this.boundryEnd + this.boundry >= this.pages) {
+        this.boundryEnd = this.pages;
+        this.currentPageNumber = this.boundryEnd;
+      } else {
+        this.boundryEnd = this.boundryEnd + this.boundry;
+        this.currentPageNumber = this.boundryEnd;
+      }
+    }
+    //this.boundryEnd = this.pages;
+    //this.boundryStart = this.pages - this.boundry + 1;
+    //this.generateCount();
+    this.setDisplayPageNumberRange();
+    //this.getAllRoles();
+  }
+
+  backPage() {
+    if (this.currentPos - this.perPage >= 0) {
+      this.currentPos -= this.perPage;
+      this.currentPageNumber--;
+
+      // this.boundryStart--;
+      // this.boundryEnd--;
+      // this.generateCount();
+      this.setDisplayPageNumberRange();
+      //this.getAllRoles();
+    }
+    else {
+      this.currentPos = 0;
+      this.currentPageNumber = 1;
+    }
+  }
+  nextPage() {
+    if (this.currentPos + this.perPage < this.total) {
+      this.currentPos += this.perPage;
+      this.currentPageNumber++;
+      this.boundryStart++;
+      // if (this.boundryStart > this.boundryEnd) {
+      //     this.boundryStart--;
+      //     this.moreNextPages();
+      // }
+      this.setDisplayPageNumberRange();
+    //  this.getAllRoles();
+    }
+  }
+
+  pageClick(pageNumber) {
+    this.currentPos = this.perPage * (pageNumber - 1);
+    this.currentPageNumber = pageNumber;
+    this.setDisplayPageNumberRange();
+   // this.getAllRoles();
+  }
+
+  noPrevPage() {
+    if (this.currentPos > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  setDisplayPageNumberRange() {
+    this.currentPos = this.perPage * (this.currentPageNumber - 1);
+
+    if ((this.currentPageNumber * this.perPage) > this.total)
+      this.lastPage = this.total;
+    else
+      this.lastPage = this.currentPageNumber * this.perPage;
+
+    if (this.lastPage >= this.total) {
+      this.lastPage = this.total;
+    }
+
+    this.firstPageNumber = 1 + this.currentPos;
+  }
+
+  noFwrdPage() {
+    if (this.currentPos + this.perPage < this.total) {
+      return true;
+    }
+    return false;
+  }
+
+  /* Pagination Function's Ends */
+
+  /* Filtering, Sorting, Search functions Starts*/
+  searchString(searchString) {
+    if (searchString == '') {
+      this.searchQuery = '';
+      this.searchCountQuery = '';
+    } else {
+      this.searchQuery = '&filter[where][displayName][like]=%' + searchString + '%';
+      this.searchCountQuery = '&[where][displayName][like]=' + searchString + '%';
+    }
+    this.currentPos = 0;
+    this.currentPageNumber = 1;
+    this.boundryStart = 1;
+    this.boundry = 3;
+    this.boundryEnd = this.boundry;
+    //this.getQueryDataCount();
+    //this.getAllSchools();
+  }
+
+  sort(column, sortOrder) {
+    if (sortOrder) {
+      this.sortUrl = '&filter[order]=' + column + ' DESC';
+    } else {
+      this.sortUrl = '&filter[order]=' + column + ' ASC';
+    }
+   // this.getAllRoles();
+  }
+
+
 }
