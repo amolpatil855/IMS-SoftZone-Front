800902672c52bbd778d0b29f6cb9120d98827af8
amolpatil855 <amol.patil@eternussolutions.com>
2018-01-18 18:46:50 +0530

    Changs done for rolelisting paging

--- src/app/theme/pages/default/features/roles/role-list/role-list.component.ts
+++ src/app/theme/pages/default/features/roles/role-list/role-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
-import { ConfirmationService,DataTableModule } from 'primeng/primeng';
+import { ConfirmationService, DataTableModule, LazyLoadEvent } from 'primeng/primeng';
 import { GlobalErrorHandler } from '../../../../../../_services/error-handler.service';
 import { MessageService } from '../../../../../../_services/message.service';
 import { RoleService } from '../../../_services/role.service';
@@ -15,14 +15,11 @@ import { Helpers } from "../../../../../../helpers";
   encapsulation: ViewEncapsulation.None,
 })
 export class RoleListComponent implements OnInit {
-  roleList= [];
-  longList:boolean;
-  total: number;         //Number Of records
-  currentPos: number;    //Current Page
-  perPage: number;       //Number of records to be displayed per page
-  firstPageNumber: number;
-  lastPage: number;
-  currentPageNumber: number;
+  roleList = [];
+  pageSize=50;
+  page=1;
+  totalCount=0;
+  search='';
   cols: any[];
   constructor(private router: Router,
     private roleService: RoleService,
@@ -34,54 +31,59 @@ export class RoleListComponent implements OnInit {
   ngOnInit() {
 
     //Page Size Array
-    this.cols = [
-      { field: 'roleName', header: 'Role Name' },
-      { field: 'roleDescription', header: 'Role Description' },
-  ];
-      this.getRoleLIst();
+    this.getRoleList();
   }
 
   onEditClick(role: Role) {
-    this.roleService.perPage = this.perPage;
-    this.roleService.currentPos = this.currentPos;
-    this.roleService.currentPageNumber = this.currentPageNumber;
+    this.roleService.perPage = this.pageSize;
+    this.roleService.currentPos = this.page;
+    // this.roleService.currentPageNumber = this.currentPageNumber;
     this.router.navigate(['/features/roles/edit', role.id]);
   }
- getRoleLIst(){
-   this.roleService.getAllRoles().subscribe(
-    results => {
-     this.roleList=results;
-     if(this.roleList.length>0)
-     {
-      this.longList=true;
-     }
-    },
-    error => {
-      this.globalErrorHandler.handleError(error);
-    });
- }
+  getRoleList() {
+    this.roleService.getAllRoles(this.pageSize,this.page,this.search).subscribe(
+      results => {
+        this.roleList = results.data;
+        this.totalCount=results.totalCount;
+      },
+      error => {
+        this.globalErrorHandler.handleError(error);
+      });
+  }
 
- onDelete(role: Role) {
-  this.confirmationService.confirm({
+  loadLazy(event: LazyLoadEvent) {
+    //in a real application, make a remote request to load data using state metadata from event
+    //event.first = First row offset
+    //event.rows = Number of rows per page
+    //event.sortField = Field name to sort with
+    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+    //imitate db connection over a network
+    this.pageSize=event.rows;
+    this.page=event.first;
+    this.search=  event.globalFilter;
+    this.getRoleList()
+  }
+
+
+  onDelete(role: Role) {
+    this.confirmationService.confirm({
       message: 'Do you want to delete this record?',
       header: 'Delete Confirmation',
       icon: 'fa fa-trash',
       accept: () => {
-          this.roleService.deleteRole(role.id).subscribe(
-              results => {
-                  this.messageService.addMessage({ severity: 'success', summary: 'Success', detail: 'Record Deleted Successfully' });
-                  if ((this.currentPageNumber - 1) * this.perPage == (this.total - 1)) {
-                      this.currentPageNumber--;
-                  }
-                this.getRoleLIst();
-              },
-              error => {
-                  this.globalErrorHandler.handleError(error);
-              })
+        this.roleService.deleteRole(role.id).subscribe(
+          results => {
+            this.messageService.addMessage({ severity: 'success', summary: 'Success', detail: 'Record Deleted Successfully' });
+            this.getRoleList();
+          },
+          error => {
+            this.globalErrorHandler.handleError(error);
+          })
       },
       reject: () => {
       }
-  });
-}
+    });
+  }
 
 }
