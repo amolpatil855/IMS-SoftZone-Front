647626e0f80e869f92a1bcaf021bce19f701c4d6
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-01-19 11:31:19 +0530

    renamed SupplierAddressDetails to MstSupplierAddressDetails

--- src/app/theme/pages/default/_models/supplier.ts
+++ src/app/theme/pages/default/_models/supplier.ts
@@ -16,5 +16,5 @@ export class Supplier{
     dispatchPersonName: string;
     dispatchPersonEmail: string;
     dispatchPersonPhone: string;
-    SupplierAddressDetails: string;
+    MstSupplierAddressDetails: string;
 }
