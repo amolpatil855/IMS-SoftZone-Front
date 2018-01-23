202ddf7ffea4c59ac7bdebe1b1007f0dc7fcfdf1
amolpatil855 <amol.patil@eternussolutions.com>
2018-01-23 12:56:00 +0530

    Changs done for collection page

--- src/app/theme/pages/default/_services/common.service.ts
+++ src/app/theme/pages/default/_services/common.service.ts
@@ -16,58 +16,9 @@ export class CommonService {
     return gender;
     //return this.http.get(AppSettings.API_ENDPOINT + 'categories', AppSettings.requestOptions()).map((response: Response) => response.json());  
   }
-  getClass() {
-    //let classList = ["--select--", "1st", "2nd", "3rd"];
-    //return classList;
-    return this.http.get(AppSettings.API_ENDPOINT + 'Classes', AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
+
   getCategoryCodes() {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Schools/' + localStorage.getItem("schoolId") + '/Schoolcategories', AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'Collection/GetCategoryLookup', AppSettings.requestOptions()).map((response: Response) => response.json());
   }
-  getYear() {
-    //let year = ["--select--", "2000", "2001", "2002"];
-    //return year;
-    return this.http.get(AppSettings.API_ENDPOINT + 'Academicyears', AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
-  getBoard() {
-    //let board = ["--select--", "SSC", "CBSC", "ICSC"];
-    //return board;
-    return this.http.get(AppSettings.API_ENDPOINT + 'Boards', AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
-  getBloodGroup() {
-    let blooDgroup = ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"];
-    return blooDgroup;
-    //return this.http.get(AppSettings.API_ENDPOINT + 'categories', AppSettings.requestOptions()).map((response: Response) => response.json());  
-  }
-
-  getZoneList() {
-    //let zoneGroup = ["Zone 1", "Zone 2", "Zone 3"];
-    //return zoneGroup;
-    return this.http.get(AppSettings.API_ENDPOINT + 'Zones', AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
-
-  getChargeHeader() {
-    let chargeHeads = ["ChargeHead1", "ChargeHead2", "ChargeHead3", "ChargeHead4", "ChargeHead5", "ChargeHead6", "ChargeHead7", "ChargeHead8", "ChargeHead9", "ChargeHead10", "ChargeHead11"];
-    return chargeHeads;
-    //return this.http.get(AppSettings.API_ENDPOINT + 'categories', AppSettings.requestOptions()).map((response: Response) => response.json());  
-  }
-
-  getConfigurationKey() {
-    let configurationKeys = [{ "keyName": "InvoiceAcronym", "Name": "Invoice Acronym", "keyValue": "" }];
-    return configurationKeys;
-  }
-
-  getConfigurationKeySFS() {
-    let configurationKeys = [{ "keyName": "AggregatorId", "Name": "Aggregator ID", "keyValue": "" },
-    { "keyName": "AggregatorKey", "Name": "Aggregator Key", "keyValue": "" }];
-    return configurationKeys;
-  }
-
-  getAllAudit(url) {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Vwauditlogs/' + (localStorage.getItem('schoolId') === undefined ? 0 : localStorage.getItem('schoolId')) + '/getAuditDetails' + url, AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
-  getAuditCount(url) {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Vwauditlogs/' + (localStorage.getItem('schoolId') === undefined ? 0 : localStorage.getItem('schoolId')) + '/getAuditDetailsCount' + url, AppSettings.requestOptions()).map((response: Response) => response.json());
-  }
-
+ 
 }
