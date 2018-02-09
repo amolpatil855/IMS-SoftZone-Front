1886ce243e02a52065174488557e16c2f8f36777
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-02-09 19:31:02 +0530

    Added Add Edit Page

---
+++ src/app/theme/pages/default/features/purchase/trnPurchaseOrder/trnPurchaseOrder-add-edit/trnPurchaseOrder-add-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: "app-trnPurchaseOrder-add-edit",
+  templateUrl: "./trnPurchaseOrder-add-edit.component.html",
+  encapsulation: ViewEncapsulation.None,
+})
+export class TrnPurchaseOrderAddEditComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/theme/pages/default/features/purchase/trnPurchaseOrder/trnPurchaseOrder-list/trnPurchaseOrder-list.component.ts
+++ src/app/theme/pages/default/features/purchase/trnPurchaseOrder/trnPurchaseOrder-list/trnPurchaseOrder-list.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import * as _ from 'lodash/index';
+import { FormGroup, Validators, FormBuilder, FormArray, FormControl } from '@angular/forms';
+import { ConfirmationService, DataTableModule, LazyLoadEvent, SelectItem } from 'primeng/primeng';
+import { GlobalErrorHandler } from '../../../../../../../_services/error-handler.service';
+import { MessageService } from '../../../../../../../_services/message.service';
+import { TrnPurchaseOrderService } from '../../../../_services/trnPurchaseOrder.service';
+import { ScriptLoaderService } from '../../../../../../../_services/script-loader.service';
+import { Helpers } from "../../../../../../../helpers";
+import { TrnPurchaseOrder } from "../../../../_models/trnPurchaseOrder";
 
 @Component({
   selector: "app-trnPurchaseOrder-list",
@@ -6,11 +17,65 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class TrnPurchaseOrderListComponent implements OnInit {
-
-  constructor() {
+trnPurchaseOrderForm: any;
+  trnPurchaseOrderObj: any;
+  params: number;
+  trnPurchaseOrderList = [];
+  pageSize = 50;
+  page = 1;
+  totalCount = 0;
+  search = '';
+  tableEmptyMesssage = 'Loading...';
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private trnPurchaseOrderService: TrnPurchaseOrderService,
+    private globalErrorHandler: GlobalErrorHandler,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService) {
   }
 
   ngOnInit() {
+
   }
+
+  getTrnPurchaseOrdersList(){
+    this.trnPurchaseOrderList = [{
+      id: 1,
+      courierId: 101,
+      courierMode: '',
+      saleOrderId: 11,
+      saleOrderNumber: 0,
+      supplierId: 2,
+      orderNumber: 111,
+      
+    }];
+  }
+
+  loadLazy(event: LazyLoadEvent) {
+    //in a real application, make a remote request to load data using state metadata from event
+    //event.first = First row offset
+    //event.rows = Number of rows per page
+    //event.sortField = Field name to sort with
+    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+    //imitate db connection over a network
+    this.pageSize = event.rows;
+    this.page = event.first;
+    this.search = event.globalFilter;
+    this.getTrnPurchaseOrdersList();
+  }
+
+  onEditClick(trnPurchaseOrder: TrnPurchaseOrder) {
+        this.router.navigate(['/features/purchase/trnPurchaseOrder/edit', trnPurchaseOrder.id]);
+    }
+    onDelete(trnPurchaseOrder: TrnPurchaseOrder) {
+        
+    }
+
+    onAddClick() {
+        this.router.navigate(['/features/purchase/trnPurchaseOrder/add']);
+    }
 
 }
