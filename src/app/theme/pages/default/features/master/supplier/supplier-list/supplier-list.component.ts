82724873267b81db6f40bf8f2b4eeade269e39d6
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-02-16 19:29:46 +0530

    Added creditPeriodDays field

--- src/app/theme/pages/default/features/master/supplier/supplier-list/supplier-list.component.ts
+++ src/app/theme/pages/default/features/master/supplier/supplier-list/supplier-list.component.ts
@@ -72,6 +72,7 @@ export class SupplierListComponent implements OnInit {
       dispatchPersonName: '',
       dispatchPersonEmail: '',
       dispatchPersonPhone: '',
+      creditPeriodDays: null,
       MstSupplierAddresses: [],
     };
 
