b44f89575eceeaec57a7646f190f4147cde844af
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-03-12 22:18:59 +0530

    Implemented Material Selection Create, Update and Listing Functionality and fixed redirect issue for Gin.

---
+++ src/app/theme/pages/default/features/sales/trnMaterialSelection/trnMaterialSelection.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: ".m-grid__item.m-grid__item--fluid.m-wrapper",
+  templateUrl: "./trnMaterialSelection.component.html",
+  encapsulation: ViewEncapsulation.None,
+})
+export class TrnMaterialSelectionComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/theme/pages/default/features/sales/trnMaterialSelection/trnMaterialSelection.module.ts
@@ -0,0 +1,109 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from "../../../default.component";
+import { AuthGuard } from "../../../../../../auth/_guards/auth.guard";
+import { LayoutModule } from "../../../../../layouts/layout.module";
+import { CommonService } from '../../../_services/common.service';
+import { CollectionService } from '../../../_services/collection.service';
+import {TrnProductStockService} from "../../../_services/trnProductStock.service";
+import {
+  DataTableModule,
+  SharedModule,
+  ButtonModule,
+  AutoCompleteModule,
+  DropdownModule,
+  OverlayPanelModule,
+  DialogModule,
+  CalendarModule,
+  RadioButtonModule,
+  ConfirmDialogModule,
+  ConfirmationService,
+} from 'primeng/primeng';
+
+import { TrnMaterialSelectionService } from "../../../_services/trnMaterialSelection.service";
+import { TrnMaterialSelectionComponent } from "./trnMaterialSelection.component";
+import { TrnMaterialSelectionListComponent } from "./trnMaterialSelection-list/trnMaterialSelection-list.component";
+import { TrnMaterialSelectionAddEditComponent } from "./trnMaterialSelection-add-edit/trnMaterialSelection-add-edit.component";
+import { ShadeService } from "../../../_services/shade.service";
+import { FomSizeService } from "../../../_services/fomSize.service";
+import { MatSizeService } from "../../../_services/matSize.service";
+import { CustomerService } from "../../../_services/customer.service";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "",
+        component: TrnMaterialSelectionComponent,
+        children: [
+          {
+            path: 'list',
+            component: TrnMaterialSelectionListComponent,
+            canActivate: [AuthGuard],
+            data: {
+              permissions: ['salesorder']
+            }
+          },
+          {
+            path: 'add',
+            component: TrnMaterialSelectionAddEditComponent,
+            canActivate: [AuthGuard],
+            data: {
+              permissions: ['salesorder']
+            }
+          },
+          {
+            path: 'edit/:id',
+            component: TrnMaterialSelectionAddEditComponent,
+            canActivate: [AuthGuard],
+            data: {
+              permissions: ['salesorder']
+            }
+          },
+        ]
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forChild(routes),
+    LayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // primeng modules
+    DataTableModule,
+    SharedModule,
+    ButtonModule,
+    AutoCompleteModule,
+    DropdownModule,
+    CalendarModule,
+    OverlayPanelModule,
+    DialogModule,
+    RadioButtonModule,
+    ConfirmDialogModule
+  ],
+  declarations: [
+    TrnMaterialSelectionComponent,
+    TrnMaterialSelectionListComponent,
+    TrnMaterialSelectionAddEditComponent
+  ],
+  providers: [
+    CommonService,
+    TrnMaterialSelectionService,
+    CustomerService,
+    ShadeService,
+    FomSizeService,
+    MatSizeService,
+    ConfirmationService,
+    CollectionService,
+    TrnProductStockService
+  ],
+})
+export class TrnMaterialSelectionModule {
+}
