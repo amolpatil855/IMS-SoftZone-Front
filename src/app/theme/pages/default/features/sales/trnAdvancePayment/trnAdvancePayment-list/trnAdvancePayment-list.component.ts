a16a53bd7e7387a79df2410c54c4fad828299d05
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-03-19 12:46:18 +0530

    Added Menu Advance Payment and its components.

---
+++ src/app/theme/pages/default/features/sales/trnAdvancePayment/trnAdvancePayment-list/trnAdvancePayment-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: "app-trnAdvancePayment-list",
+  templateUrl: "./trnAdvancePayment-list.component.html",
+  encapsulation: ViewEncapsulation.None,
+})
+export class TrnAdvancePaymentListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
