96e8429902d669de4634253d082a63d8bc18b7d5
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-04-02 18:18:39 +0530

    Updated service calls.

--- src/app/theme/pages/default/_services/clientListForCustomer.service.ts
+++ src/app/theme/pages/default/_services/clientListForCustomer.service.ts
@@ -12,27 +12,27 @@ export class ClientListForCustomerService {
   currentPageNumber: any = 1;
 
   getAccessoryProducts(pageSize = 0, page = 0) {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetAccessoryProductsForCL?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetAccessoryProducts?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
   }
 
   getFabricProducts(pageSize = 0, page = 0) {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetFabricProductsForCL?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetFabricProducts?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
   }
 
   getFoamProducts(pageSize = 0, page = 0) {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetFoamProductsForCL?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetFoamProducts?pageSize=' + pageSize + '&page=' + page, AppSettings.requestOptions()).map((response: Response) => response.json());
   }
 
   getAccessoryProductsForExport() {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetAccessoryProductsForCLExport', AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetAccessoryProductsForExport', AppSettings.requestOptions()).map((response: Response) => response.json());
   }
 
   getFabricProductsForExport() {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetFabricProductsForCLExport', AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetFabricProductsForExport', AppSettings.requestOptions()).map((response: Response) => response.json());
   }
 
   getFoamProductsForExport() {
-    return this.http.get(AppSettings.API_ENDPOINT + 'Reports/GetFoamProductsForCLExport', AppSettings.requestOptions()).map((response: Response) => response.json());
+    return this.http.get(AppSettings.API_ENDPOINT + 'CustomerLogin/GetFoamProductsForExport', AppSettings.requestOptions()).map((response: Response) => response.json());
   }
   
 }
