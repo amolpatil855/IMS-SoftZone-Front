5296565f8744033912a22a5156fef5c509737bd2
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-04-09 16:45:13 +0530

    Added TrnCurtainQuotation Components.

---
+++ src/app/theme/pages/default/features/sales/trnCurtainQuotation/trnCurtainQuotation-add-edit/trnCurtainQuotation-add-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: "app-trnCurtainQuotation-add-edit",
+  templateUrl: "./trnCurtainQuotation-add-edit.component.html",
+  encapsulation: ViewEncapsulation.None,
+})
+export class TrnCurtainQuotationAddEditComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/theme/pages/default/features/sales/trnCurtainQuotation/trnCurtainQuotation-list/trnCurtainQuotation-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: "app-trnCurtainQuotation-list",
+  templateUrl: "./trnCurtainQuotation-list.component.html",
+  encapsulation: ViewEncapsulation.None,
+})
+export class TrnCurtainQuotationListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
